4a5e0e9ea08361b98bac70c67f7f8c3dd489c7d7
Diddens, Christian (UT-TNW) <c.diddens@utwente.nl>
2024-05-18 20:01:40 +0200

    Added header to the dimensional droplet case

--- bifurcations/hanging_droplet/dimensional_droplet_detach_with_marangoni.py
+++ bifurcations/hanging_droplet/dimensional_droplet_detach_with_marangoni.py
@@ -1,12 +1,36 @@
-from pyoomph.expressions.units import NPFloatArray
-from pyoomph.generic.problem import Problem
-from pyoomph.meshes.meshdatacache import MeshDataEigenModes
-from pyoomph.meshes.remesher import Remesher2d
-from pyoomph.utils.dropgeom import DropletGeometry
+#  @file
+#  @author Christian Diddens <c.diddens@utwente.nl>
+#  @author Duarte Rocha <d.rocha@utwente.nl>
+#  
+#  @section LICENSE
+# 
+#  pyoomph - a multi-physics finite element framework based on oomph-lib and GiNaC 
+#  Copyright (C) 2021-2024  Christian Diddens & Duarte Rocha
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#
+#  The authors may be contacted at c.diddens@utwente.nl and d.rocha@utwente.nl
+#
+# ========================================================================
+
+
 from pyoomph import *
 from pyoomph.equations.ALE import *
 from pyoomph.expressions.units import *
 from pyoomph.materials import *
+from pyoomph.meshes.remesher import Remesher2d
+from pyoomph.utils.dropgeom import DropletGeometry
 
 # Build droplet mesh
 class DropletMesh(GmshTemplate):
