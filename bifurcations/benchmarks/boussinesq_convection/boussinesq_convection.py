ea8186efd70a095b845a5f37ca4018a4cf4a7188
Diddens, Christian (UT-TNW) <c.diddens@utwente.nl>
2024-05-18 07:47:43 +0200

    Add headers

--- bifurcations/benchmarks/boussinesq_convection/boussinesq_convection.py
+++ bifurcations/benchmarks/boussinesq_convection/boussinesq_convection.py
@@ -1,3 +1,30 @@
+#  @file
+#  @author Christian Diddens <c.diddens@utwente.nl>
+#  @author Duarte Rocha <d.rocha@utwente.nl>
+#  
+#  @section LICENSE
+# 
+#  pyoomph - a multi-physics finite element framework based on oomph-lib and GiNaC 
+#  Copyright (C) 2021-2024  Christian Diddens & Duarte Rocha
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#
+#  The authors may be contacted at c.diddens@utwente.nl and d.rocha@utwente.nl
+#
+# ========================================================================
+
+
 # A comparison with oomph-lib's example case
 # https://oomph-lib.github.io/oomph-lib/doc/multi_physics/b_convection/html/index.html
 # oomph-lib driver code: https://oomph-lib.github.io/oomph-lib/demo_drivers/multi_physics/boussinesq_convection/
